b8e51cb6471605f9fd9f97ff885ae083fdcb424c
asukim2020 <asukim2020@gmail.com>
2020-05-04 10:47:03 +0900

    first commit

---
+++ Home/VCHomeTabBar.swift
@@ -0,0 +1,17 @@
+//
+//  VCHomeTabBar.swift
+//  ItemManagement
+//
+//  Created by Asu on 2020/05/03.
+//  Copyright © 2020 Asu. All rights reserved.
+//
+
+import UIKit
+
+class VCHomeTabBar: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
